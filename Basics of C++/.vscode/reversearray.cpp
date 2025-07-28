98e86f3223fdf7e6152bc7dabcf5c6f91f5e1fc5
Krishna <public.contact.krishna@gmail.com>
2025-07-28 22:57:13 +0530

    Reverse Array

--- Basics of C++/.vscode/reversearray.cpp
+++ Basics of C++/.vscode/reversearray.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 using namespace std;
+
 void reversearray(int arr[], int n)
 {
-    int low=0, high=n-1;
-    while(low<high)
+    int low = 0, high = n - 1;
+    while (low < high)
     {
-        int temp=arr[low];
-        arr[low]=arr[high];
-        arr[high]=temp;
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
         low++;
         high--;
     }
 }
+
 int main()
 {
-    int arr[]={3, 5, 6, 89, 23, 100};
-    reversearray(arr, 6);
-    cout<<"Reversed Array: \n";
-    for(int i=0;i<5;i++)
+    int arr[] = {3, 5, 6, 89, 23, 100};
+    int n = sizeof(arr) / sizeof(arr[0]); // Safe calculation
+
+    reversearray(arr, n);
+
+    cout << "Reversed Array:\n";
+    for (int i = 0; i < n; i++)
     {
-        cout<<arr[i];
-        cout<<"\n";
+        cout << arr[i] << "\n";
     }
 }
